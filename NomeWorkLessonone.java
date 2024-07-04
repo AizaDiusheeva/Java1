04a96fd4f48b7076902d560b13a6399ab70725ae
AizaDiusheeva <165205849+AizaDiusheeva@users.noreply.github.com>
2024-07-04 18:32:57 +0200

    Add files via upload

---
+++ NomeWorkLessonone.java
@@ -0,0 +1,12 @@
+public class NomeWorkLessonone {
+    public static void main(String[] args) {
+        System.out.println("char;G");
+        System.out.println("integer;89");
+        System.out.println("byte;4");
+        System.out.println("shot;56");
+        System.out.println("floot;4.7333436");
+        System.out.println("double;4.355453532");
+        System.out.println("long;12121");
+        System.out.println("3,4,5");
+    }
+}
