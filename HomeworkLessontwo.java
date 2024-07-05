693f96d0a263e5599cd0320dabb3685cbd085c17
AizaDiusheeva <165205849+AizaDiusheeva@users.noreply.github.com>
2024-07-05 18:57:28 +0200

    Add files via upload

---
+++ HomeworkLessontwo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class HomeworkLessontwo {
+    public static void main(String[] args) {
+        String text = "Back End";
+        System.out.println("Back End");
+        System.out.println(text.length());
+
+        String partOfString = text.substring(2, 5);
+        System.out.println("Part of string from 2 to 5 is " + partOfString);
+    }
+}
+
---
+++ lessontwo.java
@@ -0,0 +1,18 @@
+public class lessontwo {
+    public static void main(String[] args) {
+           String textTwo = "I study basic java";
+            System.out.println("I study basic java");
+            boolean result = textTwo.contains("Java");
+            System.out.println("Text contaims Java = " + result);
+            String lowwrCaceString = textTwo.toLowerCase();
+            System.out.println(lowwrCaceString);
+        System.out.println(textTwo.toUpperCase());
+        System.out.println(textTwo.toLowerCase());
+        boolean resultone = textTwo.contains("Java");
+        System.out.println("Text contains Java = " + result);
+        char temp = textTwo.charAt(0);
+        System.out.println("Letter from 1 position is " + temp);
+        }
+    }
+
+
