3b46954a48634212b4a6adda453657ad85c9a82e
AizaDiusheeva <diusheevaaiza@gmail.com>
2024-07-21 15:20:45 +0200

    Add readmi

--- src/main/java/Homeworkfive/zadacha.java
+++ src/main/java/Homeworkfive/zadacha.java
@@ -1,4 +1,23 @@
 package Homeworkfive;
 
+import java.util.Scanner;
+
 public class zadacha {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое целое число");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе целое число");
+        int num2 = scanner.nextInt();
+        System.out.println("Введите третье целое число");
+        int num3 = scanner.nextInt();
+        int max = num1;
+        if (num2 > max) {
+            max = num2;
+        }
+        if (num3 > max) {
+            max = num3;
+            System.out.println("Максимальное число" + max);
+        }
+    }
 }
