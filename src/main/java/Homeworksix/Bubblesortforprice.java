adc2a4ac9322d9dedcc07cf0f7ed2ac2dd4efb2b
AizaDiusheeva <diusheevaaiza@gmail.com>
2024-08-11 01:42:58 +0200

    Add readmi

--- src/main/java/Homeworksix/Bubblesortforprice.java
+++ src/main/java/Homeworksix/Bubblesortforprice.java
@@ -15,6 +15,25 @@ public class Bubblesortforprice {
     }
 }
 
+public static void main(Cars[] carsArray) {
+            int n = carsArray.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (carsArray[j].models.compareTo(carsArray[j + 1].models) > 0) {
+                    // Меняем местами элементы
+                    Cars temp = carsArray[j];
+                    carsArray[j] = carsArray[j + 1];
+                    carsArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+public static void printCarsArray(Cars[] carsArray) {
+    for (Cars car : carsArray) {
+        System.out.println(car);
+    }
+}
 
 
 
